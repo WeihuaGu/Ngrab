a59ff41c897f29abe0181f070674629f2317cca3
weihuagu <weihuagu_work@163.com>
2019-07-14 15:54:03 +0800

    实现获取标签文本块

--- grabtextblock.js
+++ grabtextblock.js
@@ -1,1 +1,50 @@
-var textblocktag=["div","table","tr","td","th","ul","li","dl","dt","dd"];
+const jsdom = require("jsdom");
+const {
+        JSDOM
+} = jsdom;
+var textblocktag = ["div", "table", "tr", "td", "th", "ul", "li", "dl", "dt", "dd"];
+
+var isTextBlockTag = (tagname) => {
+        if(tagname==undefined)
+                return false;
+        var tag = tagname.toLowerCase();
+        if (textblocktag.indexOf(tag) != -1)
+                return true;
+        else
+                return false;
+}
+
+var getRawRoot = (data) => {
+        var dom = new JSDOM(data);
+        return dom.window.document.body;
+}
+
+
+
+var getTextBlocksByNode = (root) => {
+        var childnodes = root.childNodes;
+        if (childnodes == undefined)
+                return ;
+        for (item in childnodes) {
+                var childitem = childnodes[item];
+                if (isTextBlockTag(childitem.tagName)) {
+                        //console.log(childitem + "是textblock节点");
+                        getTextBlocksByNode(childitem);
+                } else {
+                        var parent = childitem.parentElement;
+                        //console.log(childitem+ "不是textblock,删除它");
+                        if(parent==undefined)
+                                continue;
+                        var removed = parent.removeChild(childitem);
+                }
+
+
+        }
+
+        return root;
+}
+
+var getTextBlocks = (data) =>{
+        return getTextBlocksByNode(getRawRoot(data));
+}
+module.exports.getTextBlocks = getTextBlocks;
--- test.js
+++ test.js
@@ -1,7 +1,10 @@
+var cheerio = require("cheerio");
 var page = require('./page');
+var grabtextblock = require('./grabtextblock');
 var testurl = "https://xueqiu.com/6195589551/129583938";
 page.getCleanPage(testurl).then((data) => {
-        console.log(data);
+        var blocks=grabtextblock.getTextBlocks(data.body);
+        console.log(blocks);
 }, (err) => {
         console.log(err);
 });
